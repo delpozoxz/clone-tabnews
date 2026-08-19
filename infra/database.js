35edc3542de96efba962b18b4c52c80444f48fce
Leandro Soares Del Pozo <leandro.pozo@flai.tec.br>
2026-08-19 13:29:12 -0300

    client datbase mais robusto

--- infra/database.js
+++ infra/database.js
@@ -15,10 +15,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  try{ await client.connect();
   const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  return result;}
+    catch (error) {
+    console.error('Erro ao conectar ou consultar o banco de dados:', error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
